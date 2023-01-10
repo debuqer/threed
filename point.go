39e2feb826634b865a0d9eb94059bfe0b1573836
Mohammad Abbasi <m.abbasi@dadehpardaz.com>
2023-01-10 15:14:30 +0330

    seperate of concern

---
+++ point.go
@@ -0,0 +1,17 @@
+package main
+
+import "math"
+
+type Point struct {
+	X float64
+	Y float64
+	Z float64
+}
+
+func (a *Point) DistanceToCordination(x float64, y float64, z float64) float64 {
+	return math.Sqrt(math.Pow(a.X-x, 2) + math.Pow(a.Y-y, 2) + math.Pow(a.Z-z, 2))
+}
+
+func (a *Point) DistanceToPoint(b Point) float64 {
+	return a.DistanceToCordination(b.X, b.Y, b.Z)
+}
